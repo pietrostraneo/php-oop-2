2ec023cd5ca733d3f84cf4d9ee6f309aa94e96c9
pietrostraneo <straneo_p@yahoo.it>
2024-01-31 17:41:43 +0100

    Created db.php file and imported all model files

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php 
+
+    require_once __DIR__.'/Model/animalBreed.php';
+    require_once __DIR__.'/Model/petProducts.php';
+    require_once __DIR__.'/Model/petFood.php';
+    require_once __DIR__.'/Model/petGames.php';
+    require_once __DIR__.'/Model/petAccessory.php';
+
+?>
