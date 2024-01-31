dfb34d0055a38e209b832c1b5cda35b27f63273f
pietrostraneo <straneo_p@yahoo.it>
2024-01-31 17:54:36 +0100

    Added new instances in db file

--- db.php
+++ db.php
@@ -6,4 +6,12 @@
     require_once __DIR__.'/Model/petGames.php';
     require_once __DIR__.'/Model/petAccessory.php';
 
+
+    $dogs = new Breed('Dogs');
+    $cats = new Breed('Cats');
+    $fishes = new Breed('Fish');
+    $birds = new Breed('Birds');
+
+    $miniadult = new food('Royal Canin', 'Mini Adult', '15,00€', 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', 'Dry Food', '2 Kg', '15/12/2026');
+    
 ?>
