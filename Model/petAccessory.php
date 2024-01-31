8ddb72a71389ce730f9af9c151def47cf3b951c9
pietrostraneo <straneo_p@yahoo.it>
2024-01-31 17:35:42 +0100

    Created class for pet Accessories

---
+++ Model/petAccessory.php
@@ -0,0 +1,20 @@
+<?php
+
+    require_once __DIR__.'/petProducts.php';
+
+    class games extends products{
+
+        public $size;
+        public $weight;
+
+        function __construct($_company,$_name,$_price,$_img,$_size,$_weight){
+            
+            parent::__construct($_company,$_name,$_price,$_img);
+            $this->size = $_size;
+            $this->weight = $_weight;
+
+        }
+
+    }
+
+?>
